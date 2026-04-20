eb5604b9b8fbc5cf4bb9774f63e54b6501efe43e
Carl Hellstrom <carl.f.hellstrom@gmail.com>
2026-04-20 15:27:56 +0200

    Revised the database to contain easier words for the game.

--- app/src/main/java/com/example/owo/mamn01project/DatabaseHelper.java
+++ app/src/main/java/com/example/owo/mamn01project/DatabaseHelper.java
@@ -16,7 +16,7 @@ import java.util.List;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "lth_words.db";
-    private static final int DATABASE_VERSION = 2;
+    private static final int DATABASE_VERSION = 3;
 
     private final Context context;
 
